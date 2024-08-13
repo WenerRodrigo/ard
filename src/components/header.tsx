0bb456c6cf723fac3497eddd950e3dd2d15c1fc9
WenerRodrigo <wener19.wr@gmail.com>
2024-08-12 22:30:37 -0300

    fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -28,23 +28,42 @@ const Header = () => {
   };
 
   return (
-    <div className="bg-white flex justify-between items-center px-14">
-       <div className="hidden lg:flex items-center w-full">
-        <a href="/" className="flex items-center">
-          <img src={logo} alt="Logo" className="w-20" />
-          <h1 className="font-bold">
-            Garantia para<br /> <span className="text-blue-600">motor e câmbio</span>
-          </h1>
-        </a>
-      </div>
-      <div className="hidden lg:flex items-center justify-around w-full">
-        <a href="/" className="hover:text-gray-600 font-medium">Início</a>
-        <a href="/about" className="hover:text-gray-600 font-medium">A empresa</a>
-        <a href="#beneficios" className="hover:text-gray-600 font-medium" onClick={scrollToBeneficios}>Produtos</a>
-        <a href="https://api.whatsapp.com/send?phone=+551699999999" target="_blank" className="font-medium bg-blue-400 hover:bg-blue-500 text-white px-4 py-2 rounded-md">Contato</a>
+    <>
+      <div className="bg-white flex justify-between items-center px-14">
+        <div className="hidden lg:flex items-center w-full">
+          <a href="/" className="flex items-center">
+            <img src={logo} alt="Logo" className="w-20" />
+            <h1 className="font-bold">
+              Garantia para
+              <br /> <span className="text-blue-600">motor e câmbio</span>
+            </h1>
+          </a>
+        </div>
+        <div className="hidden lg:flex items-center justify-around w-full">
+          <a href="/" className="hover:text-gray-600 font-medium">
+            Início
+          </a>
+          <a href="/about" className="hover:text-gray-600 font-medium">
+            A empresa
+          </a>
+          <a
+            href="#beneficios"
+            className="hover:text-gray-600 font-medium"
+            onClick={scrollToBeneficios}
+          >
+            Produtos
+          </a>
+          <a
+            href="https://api.whatsapp.com/send?phone=+551699999999"
+            target="_blank"
+            className="font-medium bg-blue-400 hover:bg-blue-500 text-white px-4 py-2 rounded-md"
+          >
+            Contato
+          </a>
+        </div>
       </div>
       <MenuMobile />
-    </div>
+    </>
   );
 };
 
